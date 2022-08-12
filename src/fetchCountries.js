62a5b46fe9567f90b5bf820e935597a1a12b35db
RomanUstychenko <roman.ustychenko@gmail.com>
2022-08-12 09:05:16 +0300

    upd12/08

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,12 @@
 const BASE_URL = "https://restcountries.com/v3.1";
 export function fetchCountries(name) {
-    fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-.then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+    return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
 }
+
 // export default {fetchCountries}
--- src/index.js
+++ src/index.js
@@ -7,20 +7,37 @@ const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box')
 const ul = document.querySelector('.country-list')
 const div = document.querySelector('.country-info')
+const body = document.querySelector('body')
 
 
 let currentPage = 1;
-input.addEventListener('input', debounce(search, DEBOUNCE_DELAY))
+
+input.addEventListener('input', debounce(search, DEBOUNCE_DELAY)
+)
+
 
 function search (e) {
 e.preventDefault();
-let countryName = input.value;
-fetchCountries(countryName)
-// .then((response) => response.json())
-// .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+let name = input.value;
+
+fetchCountries(name)
+.then((users) => renderUserList(users))
+    .catch((error) => console.log(error));
 }
+
+
+function renderUserList(data) {
+    const markup = data
+      .map((item) => {
+        return `<li>    
+            <img src="${item.flags.svg}" width="40px" alt="${item.name}">
+            <h2>${item.name.official}</h2>
+            <p>Capital</b>: ${item.capital}</p>
+            <p>Population</b>: ${item.population}</p>
+            <p>Languages</b>: ${(Object.values(item.languages))}</p>
+        </li>`;
+      })
+      .join("");
+      div.innerHTML = markup;
+  }
+//   console.log(renderUserList(data))
